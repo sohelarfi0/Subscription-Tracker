9aeb75f68978a436b95093ad1de511fe58edf926
sohel <sohelarfi0@gmail.com>
2025-10-27 10:57:17 +0530

    day-1

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 import express from 'express';
+import {PORT} from './config/env.js';
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
+
+
 
 const app=express();
+
+app.use('/api/v1/users',userRouter);
+app.use('/api/v1/auth',authRouter);
+app.use('/api/v1/subscriptions',subscriptionRouter);
 
 app.get('/',(req,res)=>{
     res.send('Wecome to the Subcription Tracker API!');
 
 });
 
-app.listen(3000,()=>{
-    console.log('Subscription Tracker API is running on http://localhost:3000');
+app.listen(PORT,()=>{
+    console.log('Subscription Tracker API is running on http://localhost:${PORT}');
 
 })
 
---
+++ routes/subscription.routes.js
@@ -0,0 +1,20 @@
+import { Router } from "express"; 
+
+const subscriptionRouter=Router();
+
+subscriptionRouter.get('/',(req,res)=> res.send({title:'GET all subscriptions'}));
+subscriptionRouter.get('/:id',(req,res)=> res.send({title:'GET all subscription details'}));
+
+subscriptionRouter.post('/',(req,res)=> res.send({title:'CREATE subscription'}));
+
+subscriptionRouter.put('/:id',(req,res)=> res.send({title:'UPDATE subscription'}));
+
+subscriptionRouter.delete('/',(req,res)=> res.send({title:'DELETE subscription'}));
+
+subscriptionRouter.get('/user/:id',(req,res)=> res.send({title:'GET all user subscriptions'}));
+
+subscriptionRouter.put('/:id/cancel',(req,res)=> res.send({title:'CANCEL subscription'}));
+
+subscriptionRouter.get('/upcoming-renewals',(req,res)=> res.send({title:'GET all upcoming renewals'}));
+
+export default subscriptionRouter;
