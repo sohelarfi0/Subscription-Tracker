e74effbc025724912601619317c13b9cfded6b18
sohel <sohelarfi0@gmail.com>
2025-11-02 10:40:39 +0530

    day-3

--- config/env.js
+++ config/env.js
@@ -7,4 +7,7 @@ export const {
     NODE_ENV,
     DB_URI,
     JWT_SECRET,
-    JWT_EXPIRES_IN}=process.env;
+    JWT_EXPIRES_IN,
+    ARCJET_KEY,
+    ARCJET_ENV
+}=process.env;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-
+import { getUser ,getUsers} from "../controller/user.controller";
+import authorize from "../middleware/auth.middleware";
 const userRouter=Router();
-userRouter.get('/',(req,res)=>res.send({title:'Get all users'}));
-userRouter.get('/:id',(req,res)=>res.send({title:'Get user details'}));
+userRouter.get('/',getUsers);
+userRouter.get('/:id',authorize,getUser);
 userRouter.post('/',(req,res)=>res.send({title:'Create new user'}));
 userRouter.put('/:id',(req,res)=>res.send({title:'Update user'}));
 userRouter.delete('/:id',(req,res)=>res.send({title:'Delete user'}));
